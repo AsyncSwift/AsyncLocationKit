29ec933af643d19f3a117274d1bb66637b2d9334
Pavel <pav.gre4ixin@yandex.ru>
2022-01-05 00:59:35 +0300

    Add commentary

--- Sources/AsyncLocationKit/Extensions/Array+Extensions.swift
+++ Sources/AsyncLocationKit/Extensions/Array+Extensions.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+//MARK: - Array extension equal to filter, but shortly 🙂
 extension Array where Element == AnyLocationPerformer {
     func allWith(identifier: ObjectIdentifier) -> [Element] {
         return self.filter({ $0.typeIdentifier == identifier })
