2a7c1614d37dc2bbc5f569936bf7f1ccab84b45f
Pavel <pav.gre4ixin@yandex.ru>
2022-10-30 00:37:25 +0300

    Check app clip

--- Tests/AsyncLocationKitTests/AsyncLocationKitTests.swift
+++ Tests/AsyncLocationKitTests/AsyncLocationKitTests.swift
@@ -1,5 +1,24 @@
 import XCTest
+import CoreLocation
 @testable import AsyncLocationKit
 
 final class AsyncLocationKitTests: XCTestCase {
+    let locationManager = AsyncLocationManager(locationManager: MockLocationManager(), desiredAccuracy: .bestAccuracy)
+    
+    func testRequestLocation() async {
+        do {
+            let location = try await locationManager.requestLocation()
+            
+            switch location {
+            case .didUpdateLocations(let locations):
+                print(locations)
+                XCTAssert(true)
+            default:
+                XCTAssert(false, "Something went wrong")
+            }
+            
+        } catch {
+            XCTAssert(false, error.localizedDescription)
+        }
+    }
 }
