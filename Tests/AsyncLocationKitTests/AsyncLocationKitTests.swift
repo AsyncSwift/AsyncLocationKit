b5418a317b7e7f59c1d741e0ba4ceb0777f99ffa
Andrew Dodd <47298244+AndrewDodd42@users.noreply.github.com>
2022-11-21 09:11:46 +0000

    Added test of desired accuracy setting.

--- Tests/AsyncLocationKitTests/AsyncLocationKitTests.swift
+++ Tests/AsyncLocationKitTests/AsyncLocationKitTests.swift
@@ -3,10 +3,21 @@ import CoreLocation
 @testable import AsyncLocationKit
 
 final class AsyncLocationKitTests: XCTestCase {
-    let locationManager = AsyncLocationManager(locationManager: MockLocationManager(), desiredAccuracy: .bestAccuracy)
+    static let mockLocationManager = MockLocationManager()
+    
+    func testDesiredAccuracy() {
+        let firstAccuracy: LocationAccuracy = .nearestTenMetersAccuracy
+        let locationManager = AsyncLocationManager(locationManager: AsyncLocationKitTests.mockLocationManager, desiredAccuracy: firstAccuracy)
+        XCTAssertTrue(AsyncLocationKitTests.mockLocationManager.desiredAccuracy == firstAccuracy.convertingAccuracy)
+
+        let secondAccuracy: LocationAccuracy = .bestForNavigationAccuracy
+        locationManager.updateAccuracy(with: secondAccuracy)
+        XCTAssertTrue(AsyncLocationKitTests.mockLocationManager.desiredAccuracy == secondAccuracy.convertingAccuracy)
+    }
     
     func testRequestLocation() async {
         do {
+            let locationManager = AsyncLocationManager(locationManager: AsyncLocationKitTests.mockLocationManager)
             let location = try await locationManager.requestLocation()
             
             switch location {
